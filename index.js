c0a1af4db4bb158f9340e9ea76e6a1d0cd634e40
knightsean00 <knightsean00@gmail.com>
2020-06-09 18:41:06 -0700

    Finishing up sections and ui

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 $(window).scroll(function () {
-    console.log($(window).scrollTop())
     if ($(window).scrollTop() > 0) {
         $(".header").addClass("header-transition");
         $(".hamburger").addClass("hamburger-transition");
@@ -10,7 +9,34 @@ $(window).scroll(function () {
         $(".hamburger").removeClass("hamburger-transition");
         $(".header-text").removeClass("header-text-transition");
     }
+
+    if ($(window).scrollTop() > $(window).height() && $(window).scrollTop() < $(window).height() * 2) {
+        $(".about-me-link").addClass("active");
+    } else {
+        $(".about-me-link").removeClass("active");
+    }
+
+    if ($(window).scrollTop() > $(window).height() * 2 && $(window).scrollTop() < $(window).height() * 3) {
+        $(".projects-link").addClass("active");
+    } else {
+        $(".projects-link").removeClass("active");
+    }
 });
+
+$(".about-me-link").click(function() {
+    $(".menu").removeClass("menu-display");
+    document.getElementById("about").scrollIntoView({behavior: "smooth", block: "start"});
+    // $(".projects-link").removeClass("active");
+    // $(".about-me-link").addClass("active");
+});
+$(".projects-link").click(function () {
+    $(".menu").removeClass("menu-display");
+    document.getElementById("projects").scrollIntoView({ behavior: "smooth", block: "start" });
+    // $(".about-me-link").removeClass("active");
+    // $(".projects-link").addClass("active");
+});
+
+
 
 function menuToggle() {
     if($(".menu").hasClass("menu-display")) {
@@ -32,49 +58,3 @@ function menuToggle() {
     }
 }
 
-
-
-
-var colors = ['#3498db', '#1abc9c', '#FF8484'];
-
-function choose() {
-    let index = Math.floor(colors.length * Math.random());
-
-    // document.getElementById("hero").style.backgroundColor = colors[index];
-    let waves = document.getElementsByClassName("wave");
-    var i;
-    for(i = 0; i < waves.length; i++) {
-        waves[i].style.fill = colors[index];
-    }
-
-    // document.getElementById("color").innerHTML = " " + colors[index];
-}
-
-$("#parallax").mousemove(function (e) {
-    parallaxIt(e, "#wave", -100);
-});
-
-function parallaxIt(e, target, movement) {
-    var $this = $("#parallax");
-    var relX = e.pageX - $this.offset().left;
-    var relY = e.pageY - $this.offset().top;
-
-    TweenMax.to(target, 1, {
-        x: (relX - $this.width() / 2) / $this.width() * movement,
-        y: (relY - $this.height() / 2) / $this.height() * movement
-    });
-}
-
-
-// function waveGen() {
-//     humps = 5;
-//     height = .1 * screen.height;
-//     width = screen.width;
-
-//     output = ""
-
-//     var i;
-//     for(i = 0; i < humps; i++) {
-
-//     }
-// }
