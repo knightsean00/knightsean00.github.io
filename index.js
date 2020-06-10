1a13be9da091c0e3c40e1989989200658f28d31e
Sean Knight <knigh@DESKTOP-4GJ1BFI.mit.edu>
2020-06-09 22:38:28 -0700

    Fix iOS?

--- index.js
+++ index.js
@@ -12,28 +12,39 @@ $(window).scroll(function () {
 
     if ($(window).scrollTop() > $(window).height() && $(window).scrollTop() < $(window).height() * 2) {
         $(".about-me-link").addClass("active");
+        $(".about-me-menu-link").addClass("active-menu");
     } else {
         $(".about-me-link").removeClass("active");
+        $(".about-me-menu-link").removeClass("active-menu");
     }
 
     if ($(window).scrollTop() > $(window).height() * 2 && $(window).scrollTop() < $(window).height() * 3) {
         $(".projects-link").addClass("active");
+        $(".projects-menu-link").addClass("active-menu");
     } else {
         $(".projects-link").removeClass("active");
+        $(".projects-menu-link").removeClass("active-menu");
     }
 });
 
 $(".about-me-link").click(function() {
+    document.getElementById("about").scrollIntoView({behavior: "smooth", block: "start"});
+});
+$(".about-me-menu-link").click(function() {
     $(".menu").removeClass("menu-display");
     document.getElementById("about").scrollIntoView({behavior: "smooth", block: "start"});
-    // $(".projects-link").removeClass("active");
-    // $(".about-me-link").addClass("active");
 });
+
+$(".scroll-down").click(function() {
+    document.getElementById("about").scrollIntoView({behavior: "smooth", block: "start"});
+});
+
 $(".projects-link").click(function () {
+    document.getElementById("projects").scrollIntoView({ behavior: "smooth", block: "start" });
+});
+$(".projects-menu-link").click(function () {
     $(".menu").removeClass("menu-display");
     document.getElementById("projects").scrollIntoView({ behavior: "smooth", block: "start" });
-    // $(".about-me-link").removeClass("active");
-    // $(".projects-link").addClass("active");
 });
 
 
@@ -41,6 +52,7 @@ $(".projects-link").click(function () {
 function menuToggle() {
     if($(".menu").hasClass("menu-display")) {
         $(".menu").removeClass("menu-display");
+        $(".scroll-down").addClass("bob");
         // $('html, body').css({
         //     overflow: 'auto',
         //     height: 'auto'
@@ -49,6 +61,7 @@ function menuToggle() {
 
     } else {
         $(".menu").addClass("menu-display");
+        $(".scroll-down").removeClass("bob");
         // $('html, body').css({
         //     overflow: 'hidden',
         //     height: '100%'
