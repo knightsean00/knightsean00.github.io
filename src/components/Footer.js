2d5fed624fdf4dcab35c38f634f3eeacab7db031
Sean Knight <knightsean00@gmail.com>
2021-02-12 20:58:28 -0800

    Add return to top and update README

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ class Header extends React.Component {
             <footer className="pt-5 pb-5">
                 <div className="container pl-2 pr-2 text-align-center">
                     <a href="https://github.com/knightsean00/knightsean00.github.io"><h5>Source</h5></a>
-                    
+                    <a onClick={() => window.scrollTo(0,0)}><h6>To the top</h6></a>
                 </div>
             </footer>
         )
